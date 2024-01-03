690d889205eb8498926b4b2a2bc325666b23674c
Elshaabany <elshaabany@protonmail.com>
2024-01-03 03:18:07 +0200

    section 4: Thread Communication Exercises

--- src/main/scala/lectures/part3concurrency/ThreadCommunication.scala
+++ src/main/scala/lectures/part3concurrency/ThreadCommunication.scala
@@ -213,7 +213,81 @@ object ThreadCommunication extends App {
     (1 to nProducers).foreach(i => new Producer(i, buffer, capacity).start())
   }
 
-  multiProdCons(3, 6)
+//  multiProdCons(3, 6)
 
+  /*
+      Exercises.
+      1) think of an example where notifyALL acts in a different way than notify?
+      2) create a deadlock
+      3) create a livelock
+     */
 
+  def testNotifyAll(): Unit = {
+    val bell = new Object
+
+    (1 to 10).foreach(i => new Thread(() => {
+      bell.synchronized {
+        println(s"[thread $i] waiting...")
+        bell.wait()
+        println(s"[thread $i] hooray!")
+      }
+    }).start())
+
+    new Thread(() => {
+      Thread.sleep(2000)
+      println("[announcer] Rock'n roll!")
+      bell.synchronized {
+        bell.notifyAll()
+      }
+    }).start()
+  }
+  // notify all will awake all sleeping threads
+  // notify will awake only one thread
+
+//  testNotifyAll()
+
+  // 2 - deadlock
+  case class Friend(name: String) {
+
+    def bow(other: Friend) = {
+      this.synchronized {
+        println(s"$this: bowing to my friend $other")
+        other.rise(this)
+        println(s"$this: my friend $other has risen")
+      }
+    }
+
+    def rise(other: Friend) = {
+      this.synchronized {
+        println(s"$this: rising to my friend $other")
+      }
+    }
+
+    var side = "right"
+
+    def switchSide() = {
+      if (side == "right") side = "left"
+      else side = "right"
+    }
+
+    def pass(other: Friend) = {
+      while (this.side == other.side) {
+        println(s"$this please $other feel free to pass")
+        switchSide()
+        Thread.sleep(1000)
+      }
+      println(s"$this passed, thanks to $other")
+    }
+  }
+
+  val sam = Friend("Sam")
+  val pierre = Friend("pierre")
+
+//  new Thread(() => sam.bow(pierre)).start()
+//  new Thread(() => pierre.bow(sam)).start()
+
+  // 3 - livelock
+
+  new Thread(() => sam.pass(pierre)).start()
+  new Thread(() => pierre.pass(sam)).start()
 }
